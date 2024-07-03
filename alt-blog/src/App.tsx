abb167a4c6286a16bc25144d099435b54cc18d9e
Alexandra J. <158613483+AlexandraJUREDIEU@users.noreply.github.com>
2024-07-03 12:12:05 +0200

    ➕ add react-router-dom
    Setting up the Browser and adding “Routes”.
    Creating a Navbar Component.

--- alt-blog/src/App.tsx
+++ alt-blog/src/App.tsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home/Home'
+import Blog from './pages/Blog/Blog'
+import Add_article from './pages/Add_article/Add_article'
+
 
 function App() {
   
 
   return (
     <>
-
+    <Routes>
+        <Route path="/" element={<Home/>}>Accueil</Route>
+        <Route path="/blog" element={<Blog/>}>Blog</Route>
+        <Route path="/add-article" element={<Add_article/>}>Ajouter un article</Route>
+    </Routes>
+    <h1>Hello World !!</h1>
+    
     </>
   )
 }
